242c655ea71f5ab47fea95a898854409836a0f4c
Meetpatel-09 <pmeet007m@gmail.com>
2021-12-06 22:24:39 +0530

    Static View complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,70 @@
-import 'package:cource_app_ui/pages/home_page.dart';
+import 'package:cource_app_ui/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+  final screens = [
+    const HomePage(),
+    const Center(child: Text('Exam', style: TextStyle(fontSize: 55),),),
+    const Center(child: Text('My Exam', style: TextStyle(fontSize: 55),),),
+    const Center(child: Text('Profile', style: TextStyle(fontSize: 55),),),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+          body: SingleChildScrollView(
+            child: IndexedStack(
+              index: currentIndex,
+              children: screens,
+            ),
+          ),
+        bottomNavigationBar: BottomNavigationBar(
+          // type: BottomNavigationBarType.fixed,
+          // selectedItemColor: Colors.lightBlue,
+          unselectedItemColor: Colors.lightBlue,
+          fixedColor: Colors.lightBlue,
+          showUnselectedLabels: true,
+          currentIndex: currentIndex,
+
+          onTap: (index) => setState(() => currentIndex = index),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Colors.lightBlue,),
+                label: 'Home',
+                backgroundColor: Colors.white
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.paste, color: Colors.lightBlue,),
+                label: 'Exam',
+                backgroundColor: Colors.white
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.receipt_sharp, color: Colors.lightBlue,),
+                label: 'My Exam',
+                backgroundColor: Colors.white
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person, color: Colors.lightBlue,),
+                label: 'Profile',
+                backgroundColor: Colors.white
+            ),
+          ],
+        )
+      )
     );
   }
 }
---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:cource_app_ui/pages/home_page/widgets/details_card.dart';
+import 'package:cource_app_ui/pages/home_page/widgets/search_bar.dart';
+import 'package:cource_app_ui/pages/home_page/widgets/share_box.dart';
+import 'package:cource_app_ui/pages/home_page/widgets/sliding_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // List<Widget> columnItems = const [
+    //   Center(child: Text('HOME', style: TextStyle(fontSize: 24),)),
+    //   SizedBox(height: 20,),
+    //   SearchBar(),
+    //   SizedBox(height: 20,),
+    //   ShareBox(),
+    //   SizedBox(height: 20,),
+    //   SlidingButtons(),
+    // ];
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: const [
+            Center(child: Text('HOME', style: TextStyle(fontSize: 24),)),
+            SizedBox(height: 20,),
+            SearchBar(),
+            SizedBox(height: 20,),
+            ShareBox(),
+            SizedBox(height: 20,),
+            SlidingButtons(),
+            SizedBox(height: 20,),
+            DetailsCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
